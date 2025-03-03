3786b5c6a186fd719763e703836edf9f0ad7a4e5
mdsad <mdsad@LAPTOP-CUL491K4>
2025-03-03 11:55:24 +0530

    commit

---
+++ src/test/java/createContactTest/CreateContact.java
@@ -0,0 +1,11 @@
+package createContactTest;
+
+public class CreateContact {
+	public static void main(String[] args) {
+		System.out.println("Saif");
+		System.out.println("Saddam");
+		System.out.println("Sadan");
+		System.out.println("Sama");
+	}
+
+}
