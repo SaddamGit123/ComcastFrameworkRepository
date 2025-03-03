ef73ca49620ff5e8173b13278cfd1effb4368b7a
mdsad <mdsad@LAPTOP-CUL491K4>
2025-03-03 23:17:02 +0530

    This changes are made by Test Lead

--- src/test/java/createContactTest/CreateOrg.java
+++ src/test/java/createContactTest/CreateOrg.java
@@ -5,6 +5,7 @@ public class CreateOrg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("creatOrgPage");
+		System.out.println("This changes are made by Test Lead");
 
 	}
 
